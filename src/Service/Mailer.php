40b429151b661a7fe43d113f985dd476d3b4d263
Clivern <hello@clivern.com>
2022-09-11 20:53:18 +0200

    update

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -9,15 +9,45 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+
 /**
  * Mailer Service.
  */
 class Mailer
 {
     /**
+     * @var MailerInterface
+     */
+    private $mailer;
+
+    /**
      * Class Constructor.
      */
-    public function __construct()
+    public function __construct(MailerInterface $mailer)
     {
+        $this->mailer = $mailer;
+    }
+
+    /**
+     * Send a Message.
+     */
+    public function send(
+        string $from,
+        string $to,
+        string $subject,
+        string $template,
+        array $data = []
+    ): void {
+        $email = (new TemplatedEmail())
+            ->from($from)
+            ->to(new Address($to))
+            ->subject($subject)
+            ->htmlTemplate($template)
+            ->context($data);
+
+        $this->mailer->send($email);
     }
 }
